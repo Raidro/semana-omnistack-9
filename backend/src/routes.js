14aff68cd5df3f7ef58d9bb22604e76c917ba4e5
Mateus <mateus_sat@hotmail.com>
2019-10-03 15:25:08 -0300

    adcionado o mongo e criado o models e controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,3 +2,21 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.post('/users', (req,  res) => {   
+    //rotas sempre no plural
+    // req.query = acessar query params (para filtros);
+    // req.params = acessar route params (para edição, delete);
+    // req.bady = acessar corpo da requisição (para criação, edição);
+    
+    return res.json({ id: req.body });
+    //quando se trabalha cm json sempre tem que passar um objeto ou array;
+    //estou enviand uma propriedade(message) com um valor(hello world);
+});
+//o primiero parametro é a rota, (qual rota quero executar);
+//o segundo parametro é uma função;
+//pode usar uma arrow função, q é uma forma reduzida;
+// passando 2 parametros, (req e res);
+//o req representa a requisição, pode pegar qualquer tipo de parametro que o usuario passar;
+//e o res devlve uma resposta ao cliente (navegador);
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,30 +4,25 @@ const express = require('express');
 // o require é usado para importar uma dependencia externa;
 // é constante pq não muda;
 
+const mongoose = require('mongoose');
+
+const routes = require('./routes')
+//necessario colocar o caminho relativo;
+//sem o "./" ele vai atrás de alguma dependencia
+
 const app = express();
 // express é uma função;
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-hzhoe.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 //estou falando ao express pra utilizar formato json
 
-app.post('/users', (req,  res) => {   
-    //rotas sempre no plural
-    // req.query = acessar query params (para filtros);
-    // req.params = acessar route params (para edição, delete);
-    // req.bady = acessar corpo da requisição (para criação, edição);
-    
-    return res.json({ id: req.body });
-    //quando se trabalha cm json sempre tem que passar um objeto ou array;
-    //estou enviand uma propriedade(message) com um valor(hello world);
-
-
-});
-//o primiero parametro é a rota, (qual rota quero executar);
-//o segundo parametro é uma função;
-//pode usar uma arrow função, q é uma forma reduzida;
-// passando 2 parametros, (req e res);
-//o req representa a requisição, pode pegar qualquer tipo de parametro que o usuario passar;
-//e o res devlve uma resposta ao cliente (navegador);
+app.use(routes);
+//precisa vir depois do express json;
 
 app.listen(3333);
 //escolho a porta que quero usar
