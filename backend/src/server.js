e504b5e53f34a8357b1969acde535634cccf121a
Mateus <mateus_sat@hotmail.com>
2019-10-03 15:01:44 -0300

    criado o arquivo de rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,11 +7,19 @@ const express = require('express');
 const app = express();
 // express é uma função;
 
-app.get('/', (req, res) => {
+app.use(express.json());
+//estou falando ao express pra utilizar formato json
 
-    return res.json({ message: 'Hello World' });
+app.post('/users', (req,  res) => {   
+    //rotas sempre no plural
+    // req.query = acessar query params (para filtros);
+    // req.params = acessar route params (para edição, delete);
+    // req.bady = acessar corpo da requisição (para criação, edição);
+    
+    return res.json({ id: req.body });
     //quando se trabalha cm json sempre tem que passar um objeto ou array;
     //estou enviand uma propriedade(message) com um valor(hello world);
+
 
 });
 //o primiero parametro é a rota, (qual rota quero executar);
