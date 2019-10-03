2485eba6356f71108afc7e6d8e29e73733830f39
Mateus <mateus_sat@hotmail.com>
2019-10-03 14:36:00 -0300

    criado o req e res

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,2 +1,26 @@
+//aqui vamos inicializar o servidor;
+
 const express = require('express');
+// o require é usado para importar uma dependencia externa;
+// é constante pq não muda;
+
+const app = express();
+// express é uma função;
+
+app.get('/', (req, res) => {
+
+    return res.send('Hello World');
+
+});
+//o primiero parametro é a rota, (qual rota quero executar);
+//o segundo parametro é uma função;
+//pode usar uma arrow função, q é uma forma reduzida;
+// passando 2 parametros, (req e res);
+//o req representa a requisição, pode pegar qualquer tipo de parametro que o usuario passar;
+//e o res devlve uma resposta ao cliente (navegador);
+
+app.listen(3333);
+//escolho a porta que quero usar
+//vou poder acessar minha aplicação pelo localhost:3333;
+
 
