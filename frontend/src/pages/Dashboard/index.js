ce0002a98a43d2cc501966f94f1d172251e526ee
Mateus <mateus_sat@hotmail.com>
2019-10-04 23:20:47 -0300

    erro: front e server nao startam juntos

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
 
 export default function Dashboard() {
+    const [spots, setSpots] = useState([]);
 
     useEffect(() => {
         async function loadSpots() {
@@ -11,9 +12,22 @@ export default function Dashboard() {
                 headers: { user_id }
             });
 
-            console.log(response.data);
+            setSpots(response.data);
         }
         loadSpots();
-    }, [])
-    return <div />
+    }, []);
+    
+    return (
+        <>
+        <ul className="spot-list"> 
+            {spots.map(spot => (
+                <li key={spot._id}>
+                    <header />
+                    <strong>{spot.company}</strong>
+                    <span>{spot.price}</span>
+                </li>
+            ))}
+        </ul>
+        </>
+    )
 }
